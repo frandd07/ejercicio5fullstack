6fc2a03b105b4c9ae8b30da969267fda8748ebc3
frandd07 <150445758+frandd07@users.noreply.github.com>
2025-01-27 18:03:21 +0100

    Update completado y revisado

--- src/app/api/evento/route.js
+++ src/app/api/evento/route.js
@@ -1,45 +1,68 @@
+import { createClient } from "@supabase/supabase-js";
 
-import { createClient } from '@supabase/supabase-js'
+const supabaseUrl = "https://eyaraedkwjgspknimmfb.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV5YXJhZWRrd2pnc3BrbmltbWZiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzY3NjI4MTgsImV4cCI6MjA1MjMzODgxOH0.6GEOM6YASqLMdFYTmNW7ceebdOKKxtVMQjUqlm8VWWg";
+const supabase = createClient(supabaseUrl, supabaseKey);
 
-const supabaseUrl = 'https://eyaraedkwjgspknimmfb.supabase.co'
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV5YXJhZWRrd2pnc3BrbmltbWZiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzY3NjI4MTgsImV4cCI6MjA1MjMzODgxOH0.6GEOM6YASqLMdFYTmNW7ceebdOKKxtVMQjUqlm8VWWg"
-const supabase = createClient(supabaseUrl, supabaseKey)
+export async function GET() {
+  const { data: contacts, error } = await supabase
+    .from("evento")
+    .select("id,titulo")
+    .order("fecha", { ascending: false });
 
-export async function GET(){
-
-const{data: contacts, error} = await supabase
-.from('evento')
-.select('id,titulo')
-.order("fecha",{ascending: true})
-
-    return new Response(JSON.stringify(contacts), {status:200})
+  return new Response(JSON.stringify(contacts), { status: 200 });
 }
 
-export async function POST(request){
-    const body = await request.json()
-    const evento = body.evento
-    const {data: postData, error} = await supabase.from("evento")
-    .insert(evento)
+export async function POST(request) {
+  const body = await request.json();
+  const evento = body.evento;
 
-    if(!error){
-        return new Response(JSON.stringify({success: "Creado con éxito"}), {status:201})
+  function isFechaValida(fecha) {
+    const fechaActual = new Date();
+    const fechaHabitual = new Date(fecha);
+
+    fechaActual.setHours(0, 0, 0, 0);
+    fechaHabitual.setHours(0, 0, 0, 0);
+
+    return fechaHabitual >= fechaActual;
+  }
+
+  if (
+    evento.titulo !== "" &&
+    evento.descripcion !== "" &&
+    evento.fecha !== "" &&
+    evento.ubicacion !== "" &&
+    isFechaValida(evento.fecha)
+  ) {
+    const { data: postData, error } = await supabase
+      .from("evento")
+      .insert(evento);
+
+    if (!error) {
+      return new Response(JSON.stringify({ success: "Creado con éxito" }), {
+        status: 201,
+      });
     }
-
-    return new Response(JSON.stringify(error) , {status:400})
-    
+  } else {
+    return new Response(JSON.stringify(error), { status: 400 });
+  }
 }
 
-export async function DELETE(request){
-    const body = await request.json()
-    const id = body.id
+export async function DELETE(request) {
+  const body = await request.json();
+  const id = body.id;
 
-    const {data: deleteData, error} = await supabase.from("evento")
+  const { data: deleteData, error } = await supabase
+    .from("evento")
     .delete()
-    .eq("id", id)
+    .eq("id", id);
 
-    if(error){
-        return new Response(JSON.stringify(error), {status:404})
-    }
+  if (error) {
+    return new Response(JSON.stringify(error), { status: 404 });
+  }
 
-    return new Response(JSON.stringify({success: "eliminado con éxito"}), {status:200})
+  return new Response(JSON.stringify({ success: "eliminado con éxito" }), {
+    status: 200,
+  });
 }
